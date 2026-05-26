15984348d2eb0f86d6a2e57aa6f23f7a7d4c57df
ghelacasas <ghelabipsu@gmail.com>
2026-05-26 08:32:33 +0800

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,3 +1,6 @@
+// Developer: Gel
+// Role: Database Engineer
+// Description: Handles database connection and disconnection
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
